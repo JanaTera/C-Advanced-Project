f46945bef1ec3b371771320aa0666ed83bdb8e09
JanaTera <89927312+JanaTera@users.noreply.github.com>
2021-09-07 03:56:00 +0200

    Add files via upload

---
+++ algorithms/encryption.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char message[50];
+    int key;
+    printf("enter the message:");
+    gets(message);
+    printf("enter the key:");
+    scanf("%d",&key);
+
+    for(int i=0;i<strlen(message);i++){
+        printf("%c",message[i]=message[i]+key);
+    }
+    
+}
