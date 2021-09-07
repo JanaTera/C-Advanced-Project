aa50bd0c10492c929345c0d416865b848b7458c5
JanaTera <89927312+JanaTera@users.noreply.github.com>
2021-09-07 03:36:03 +0200

    Add files via upload

---
+++ Pointers/manual memory managment.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*
+int *p=malloc(10*sizeof(int));
+
+   if(p==NULL){
+    printf("there is no memory allocated");
+   }
+   else{
+    printf("there is memory and we allocated for you");
+    free(p);
+   }
+*/
+}
